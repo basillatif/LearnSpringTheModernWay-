fea652216a64169ab7af3e7279d4a93aacc5c5df
Basil Latif <basillatif@MEREDITHSIPHONE.localdomain>
2019-05-01 13:13:32 -0700

    Working with all the files in one package

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -0,0 +1,14 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BubbleSortAlgorithm implements SortAlgorithm {
+
+	
+	public int[] sort(int[] numbers) {
+		// TODO Auto-generated method stub
+		return numbers;
+	}
+
+}
